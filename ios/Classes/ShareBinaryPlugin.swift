37564cd29f9c4f8105e91c0fb95e4d7443685925
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-11-30 22:32:24 +0900

    refactor: Code cleanup

--- ios/Classes/ShareBinaryPlugin.swift
+++ ios/Classes/ShareBinaryPlugin.swift
@@ -16,30 +16,32 @@ public class ShareBinaryPlugin: NSObject, FlutterPlugin {
         
         switch call.method {
         case "shareBinary":
-            let bytesTypedData = args["bytes"] as! FlutterStandardTypedData
-            let filename = args["filename"] as! String
-            
-            let cacheDir = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
-            let cacheFileURL = cacheDir.appendingPathComponent(filename)
-            try? bytesTypedData.data.write(to: cacheFileURL)
-            
-            let viewController = UIApplication.shared.delegate?.window??.rootViewController
-            let shareSheet = UIActivityViewController(activityItems: [cacheFileURL], applicationActivities: nil)
-            
-            if UIDevice.current.userInterfaceIdiom == .pad {
-                // only ipad
-                let screenSize = viewController?.view.bounds ?? .zero
-                if let popoverController = shareSheet.popoverPresentationController {
-                    popoverController.sourceView = viewController?.view
-                    popoverController.sourceRect = CGRect(
-                        x: screenSize.size.width / 2,
-                        y: screenSize.size.height - 20,
-                        width: 0,
-                        height: 0
-                    )
-                }
+            guard let bytesTypedData = args["bytes"] as? FlutterStandardTypedData else {
+                result(FlutterError(code: "InvalidArgs", message: "bytes is invalid", details: "bytes must be able to be parsed in swift"))
+                return
             }
-            viewController?.present(shareSheet, animated: true, completion: nil)
+            guard let filename = args["filename"] as? String else {
+                result(FlutterError(code: "InvalidArgs", message: "filename is invalid", details: "filename must be able to be parsed in swift"))
+                return
+            }
+            
+            let fileManager = FileManager.default
+            let urls = fileManager.urls(for: .cachesDirectory, in: .userDomainMask)
+            let cachesDirectoryUrl = urls[0]
+            
+            let directoryUrl = cachesDirectoryUrl.appendingPathComponent("share_binary", isDirectory: true)
+            let direcotryPath = directoryUrl.path
+            
+            if fileManager.fileExists(atPath: direcotryPath) {
+                try? fileManager.removeItem(atPath: direcotryPath)
+            }
+            try? fileManager.createDirectory(atPath: directoryUrl.path, withIntermediateDirectories: true)
+            
+            let fileUrl = directoryUrl.appendingPathComponent(filename)
+            let filePath = fileUrl.path
+            fileManager.createFile(atPath: filePath, contents: bytesTypedData.data)
+            
+            showShareSheet(uri: fileUrl)
             
             result(nil)
         case "shareUri":
@@ -49,27 +51,38 @@ public class ShareBinaryPlugin: NSObject, FlutterPlugin {
                 return
             }
             
-            let viewController = UIApplication.shared.delegate?.window??.rootViewController
-            let shareSheet = UIActivityViewController(activityItems: [uri], applicationActivities: nil)
-
-            if UIDevice.current.userInterfaceIdiom == .pad {
-                // only ipad
-                let screenSize = viewController?.view.bounds ?? .zero
-                if let popoverController = shareSheet.popoverPresentationController {
-                    popoverController.sourceView = viewController?.view
-                    popoverController.sourceRect = CGRect(
-                        x: screenSize.size.width / 2,
-                        y: screenSize.size.height - 20,
-                        width: 0,
-                        height: 0
-                    )
-                }
-            }
-            viewController?.present(shareSheet, animated: true, completion: nil)
+            showShareSheet(uri: uri)
             
             result(nil)
         default:
             result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    private func showShareSheet(uri: URL) {
+        guard let rootViewController = UIApplication.shared.delegate?.window??.rootViewController,
+              let view = rootViewController.view else {
+            return
+        }
+        
+        let documentInteractionController = UIDocumentInteractionController(url: uri)
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            documentInteractionController.presentOptionsMenu(
+                from: view.bounds,
+                in: view,
+                animated: true
+            )
+        } else {
+            documentInteractionController.presentOptionsMenu(
+                from: CGRect(
+                    x: view.bounds.size.width / 2,
+                    y: view.bounds.size.height - 20,
+                    width: 0,
+                    height: 0
+                ),
+                in: view,
+                animated: true
+            )
         }
     }
 }
