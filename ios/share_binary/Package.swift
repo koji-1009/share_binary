b46668d5ffe15c60a1db804e5137c5e1c5275890
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-08-07 14:30:21 +0900

    feat: Support Swift Package Manager

---
+++ ios/share_binary/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "share_binary",
+    platforms: [
+        .iOS("11.0")
+    ],
+    products: [
+        .library(name: "share-binary", targets: ["share_binary"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "share_binary",
+            dependencies: [],
+            resources: []
+        )
+    ]
+)
